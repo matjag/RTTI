18b55d800bbd54ea6608ecd95ac75f5c48e5d4f5
matjag <matejag374@student.polsl.pl>
2018-02-27 23:25:49 +0100

    Rename RTTI - Zadanie4.cpp to zad4/RTTI - Zadanie4.cpp

---
+++ zad4/RTTI - Zadanie4.cpp
@@ -0,0 +1,79 @@
+﻿// RTTI - Zadanie4.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class Animal
+{
+public:
+	Animal()
+	{
+
+	}
+	virtual void MakeSound()
+	{
+		cout << "To jest klasa animal" << endl;
+	}
+};
+
+class Dog : public Animal
+{
+public:
+	Dog()
+	{
+
+	}
+	void MakeSound()
+	{
+		cout << "Hau hau hau!!" << endl;
+	}
+};
+
+class Cat : public Animal
+{
+public:
+	Cat()
+	{
+
+	}
+	void MakeSound()
+	{
+		cout << "Miau miau miau!!" << endl;
+	}
+};
+
+Animal* GenerateAnimal()
+{
+	Animal* animalPointer = nullptr;
+	srand(time(NULL));
+	switch (rand() % 3)
+	{
+	case 0:
+		animalPointer = new Animal();
+		break;
+	case 1:
+		animalPointer = new Dog();
+		break;
+	case 2:
+		animalPointer = new Cat();
+		break;
+	}
+	return animalPointer;
+}
+
+
+int main()
+{
+	// Rozwiazanie zadania.
+	Animal* ptr = GenerateAnimal();
+	cout << "Nazwa wygenerowanej klasy: " << typeid(*ptr).name() << endl;
+	ptr->MakeSound();
+	system("pause");
+    return 0;
+}
+
