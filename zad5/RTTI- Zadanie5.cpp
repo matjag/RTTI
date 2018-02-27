614631cd4af9f7c382e4f3a4114b0209f2be4611
matjag <matejag374@student.polsl.pl>
2018-02-27 23:27:07 +0100

    Rename RTTI- Zadanie5.cpp to zad5/RTTI- Zadanie5.cpp

---
+++ zad5/RTTI- Zadanie5.cpp
@@ -0,0 +1,36 @@
+﻿// RTTI- Zadanie5.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "Animal.h"
+#include "DogLike.h"
+#include "CatLike.h"
+#include "Dog.h"
+#include "Cat.h"
+#include "GeneratorFunctions.h"
+
+using namespace std;
+
+// Rozwiazanie zadania.
+void IdentifyAnimals(Animal** animalArrayPointer)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		if (dynamic_cast<DogLike*>(animalArrayPointer[i]) != nullptr)
+		{
+			animalArrayPointer[i]->Identify();
+		}
+	}
+}
+
+int main()
+{
+	srand(time(NULL));
+	IdentifyAnimals(GenerateAnimalArray());
+
+	system("pause");
+    return 0;
+}
+
